c1e60e6da57f30ca0233516d1e8f8ef60c9e45f6
Nikola Dichev <nikola.dichev@gmail.com>
2019-07-09 17:17:06 +0300

    Solved LevelOrderTraversal challenge.
    Huffman tree doesn't work - TODO

--- InterviewPreparation/src/trees/BinaryTreeHeight.java
+++ InterviewPreparation/src/trees/BinaryTreeHeight.java
@@ -2,19 +2,6 @@ package trees;
 
 import java.util.Arrays;
 
-class Node {
-    Node left;
-    Node right;
-    int data;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
-
 /**
  * See <a href="https://www.hackerrank.com/challenges/tree-height-of-a-binary-tree/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=trees">BinaryTreeHeight at HackerRank</a>
  */
@@ -22,37 +9,21 @@ public class BinaryTreeHeight {
 
     public static void main(String[] args) {
 
-        Node case1 = new Node(3);
-        Arrays.asList(5, 2, 1, 4, 6, 7).forEach(i -> insert(case1, i));
+        BinaryNode case1 = new BinaryNode(3);
+        Arrays.asList(5, 2, 1, 4, 6, 7).forEach(i -> BinaryNodeUtils.insert(case1, i));
 
         System.out.println(height(case1)); // 3
 
-        Node case2 = new Node(15);
+        BinaryNode case2 = new BinaryNode(15);
         System.out.println(height(case2)); // 0
 
-        Node case3 =  new Node(3);
-        Arrays.asList(1, 7, 5, 4).forEach(i -> insert(case3, i));
+        BinaryNode case3 =  new BinaryNode(3);
+        Arrays.asList(1, 7, 5, 4).forEach(i -> BinaryNodeUtils.insert(case3, i));
         System.out.println(height(case3)); // 3
 
     }
 
-    public static Node insert(Node root, int data) {
-        if(root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if(data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
-
-    public static int height(Node root) {
+    public static int height(BinaryNode root) {
 
         int leftHeight = maxHeight(root.left, 0);
         int rightHeight = maxHeight(root.right, 0);
@@ -61,7 +32,7 @@ public class BinaryTreeHeight {
 
     }
 
-    public static int maxHeight(Node node, int height) {
+    public static int maxHeight(BinaryNode node, int height) {
 
         if (node == null) return height;
 
